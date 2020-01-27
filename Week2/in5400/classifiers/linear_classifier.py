fa095e802d71d227dadc4df4870d064218bf0d48
Msismail <mohe95@homtail.no>
2020-01-27 16:18:39 +0100

    Week2

--- Week2/in5400/classifiers/linear_classifier.py
+++ Week2/in5400/classifiers/linear_classifier.py
@@ -99,10 +99,10 @@ class LinearClassifier(object):
     #                           END OF YOUR CODE                              #
     ###########################################################################
     return y_pred
-  
+
   def loss(self, X_batch, y_batch, reg):
     """
-    Compute the loss function and its derivative. 
+    Compute the loss function and its derivative.
     Subclasses will override this.
 
     Inputs:
@@ -130,4 +130,3 @@ class Softmax(LinearClassifier):
 
   def loss(self, X_batch, y_batch, reg):
     return softmax_loss_vectorized(self.W, X_batch, y_batch)
-
